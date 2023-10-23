a239c1bb69b52533a4c4a759d6f8443354f929f0
ALESSANDRO MATIAS ARENA <46917132@comunidadort.edu.ar>
2023-10-23 08:58:10 -0300

    nashe

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white mt-5">
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h5>Contacto</h5>
+            <p>Dirección: 123 Calle Principal, Ciudad, País</p>
+            <p>Email: info@example.com</p>
+            <p>Teléfono: +123 456 7890</p>
+          </Col>
+          <Col md={6}>
+            <h5>Enlaces Rápidos</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="/">Inicio</a>
+              </li>
+              <li>
+                <a href="/productos">Productos</a>
+              </li>
+              <li>
+                <a href="/contacto">Contacto</a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+      <div className="text-center py-3">
+        &copy; {new Date().getFullYear()} Tu Nombre o Nombre de tu Empresa
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../App.css';
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import './Inicio.css';
 
 const Inicio = () => {
   return (
       <div className="App">
         <Navbar></Navbar>
       <header className="App-header">
-        <h1>Eco1</h1>
         <button className="start-button">
-          <Link to="/juego">Jugar</Link>
+          <Link to="/matelolsas">matelolsas</Link>
+        </button>
+        <button className="start-button">
+        <Link to="/choclias">choclias</Link>
         </button>
       </header>
+      <Footer></Footer>
     </div>
   );
 };
